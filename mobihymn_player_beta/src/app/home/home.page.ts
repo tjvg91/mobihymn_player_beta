5fc8a05a0d3978b0924db262e51643809590c3f4
timmygrococo <timothy.gandionco@rococoglobaltech.com>
2019-10-18 17:28:08 +0800

    front end for home

--- mobihymn_player_beta/src/app/home/home.page.ts
+++ mobihymn_player_beta/src/app/home/home.page.ts
@@ -1,30 +1,49 @@
 import { Component } from '@angular/core';
-import { Options } from 'ng5-slider';
+import { Options, LabelType } from 'ng5-slider';
+import * as keyJson from '../../assets/keys.json';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
-  tempoVal: number;
-  keyVal: number;
+  tempoVal = 80;
+  keyVal = 0;
+  playerVal = 70;
   tempoOptions: Options;
   keyOptions: Options;
+  playerOptions: Options;
+  keys: string[];
+  voices: any[];
 
   constructor() {
+    this.keys = keyJson.default;
     this.tempoOptions = {
-      minLimit:　60,
-      maxLimit: 120,
+      floor: 60,
+      ceil: 120,
       vertical: true,
+      hidePointerLabels: true,
       step: 1
     };
     this.keyOptions = {
-      minLimit: 0,
-      maxLimit: 11,
+      floor: 0,
+      ceil: 11,
       vertical: true,
-      step: 1
-    }
+      hidePointerLabels: true,
+      translate: (value: number, label: LabelType): string => {
+        return this.keys[value];
+      }
+    };
+    this.voices = [{}, {}, {}];
+    this.playerOptions = {
+      floor: 0,
+      ceil: 150,
+      hideLimitLabels: true,
+      hidePointerLabels: true,
+      translate: (value: number): string => {
+        return (value / 60).toFixed(0) + ':' + (value % 60).toFixed(0);
+      }
+    };
   }
-
 }
---
+++ mobihymn_player_beta/src/services/api.ts
@@ -0,0 +1,8 @@
+import { File } from "@ionic-native/file/ngx";
+
+const ASSETS = "/assets/";
+
+export class API {
+  public getSoundFont = ASSETS + "soundfonts/acoustic_grand_piano-mp3.js";
+  constructor(private file: File) {}
+}
